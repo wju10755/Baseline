f0b6c516c029417eec0cbb445befd5be414d8be5
MITS <Billu@advancestuff.com>
2023-12-16 23:19:54 -0500

    Updated Preinstalled Office Removal Module

--- New-WorkstationBaseline-v2.ps1
+++ New-WorkstationBaseline-v2.ps1
@@ -417,27 +417,20 @@ else {
 
 
 # Remove Pre-Installed Office 
-$OfficeSpinnerURL = "https://raw.githubusercontent.com/wju10755/Baseline/main/OfficeScrub-Spinner.ps1"
+$OfficeSpinnerURL = "https://advancestuff.hostedrmm.com/labtech/transfer/installers/OfficeScrub-Spinner.ps1"
 $OfficeSpinnerFile = "c:\temp\OfficeScrub-Spinner.ps1"
 $OfficeScrubURL = "https://advancestuff.hostedrmm.com/labtech/transfer/installers/OffScrubc2r.vbs"
 $OfficeScrubFile = "c:\temp\OffScrubc2r.vbs"
  
-Invoke-WebRequest -Uri $SpinnerURL -OutFile $SpinnerFile -UseBasicParsing -ErrorAction Stop 
-Start-Sleep -seconds 1
-
-    # Validate successful download by checking the file size
-    $FileSize = (Get-Item $OfficeScrubFile).Length
-    $ExpectedSize = 146093 # in bytes 
-    if ($FileSize -eq $ExpectedSize) {
-        Write-Host "Scrubbing pre-installed versions of Office..." -NoNewline
-        Start-Process -FilePath "cscript.exe" -ArgumentList "$OfficeScrubFile ALL /Quiet /NoCancel" -Wait
-        Write-Host " done." -ForegroundColor "Green"
-        Write-Log "Pre-installed versions of Microsoft Office 365 successfully uninstalled."
-        Start-Sleep -Seconds 15  
-    } else {
-        Write-Host "Failed to download Office Scrub script." -ForegroundColor "Red"
-        Write-Log "Failed to download Office Scrub script."
+Invoke-WebRequest -Uri $OfficeScrubURL -OutFile $OfficeScrubFile -UseBasicParsing -ErrorAction Stop
+if (Test-Path $OfficeScrubFile) {
+Invoke-WebRequest -Uri $OfficeSpinnerURL -OutFile $OfficeSpinnerFile -UseBasicParsing -ErrorAction Stop
+    if (Test-Path $OfficeScrubFile) {
+    Start-Process -FilePath "cscript.exe" -ArgumentList "$OfficeScrubFile ALL /Quiet /NoCancel" -Wait
     }
+} else {
+Write-Host "Office C2R Scrub utility download failed"
+}
 
 
 # Remove Microsoft OneDrive
