25bf762bca636af55ae5dec2e688f824c3e55298
MITS <Billu@advancestuff.com>
2023-12-19 15:58:14 -0500

    Updated spinner output to display 'done' once uninstall jobs are finished.

--- Remove-Office-Spinner.ps1
+++ Remove-Office-Spinner.ps1
@@ -1,29 +1,42 @@
-# Define a function to display a spinning character
+# Define a function to character
 function Show-Spinner {
     param(
         [scriptblock]$Script, # The script to run in the background
         [string]$Message = "Please wait..." # The message to display
     )
+
     # Define an array of characters to spin
     $chars = @('/', '-', '\', '|')
+
     # Initialize a counter
     $i = 0
+
     # Start the script as a job
     $job = Start-Job -ScriptBlock $Script
+
     # Write the message and the first character
     Write-Host -NoNewline "$Message $($chars[$i])"
+
     # Loop until the job is done
     while ($job.State -eq "Running") {
         # Increment the counter and wrap around if needed
         $i = ($i + 1) % $chars.Length
+
         # Move the cursor back and overwrite the character
         [Console]::SetCursorPosition([Console]::CursorLeft - 1, [Console]::CursorTop)
         Write-Host -NoNewline $chars[$i]
+
         # Sleep for a short time
         Start-Sleep -Milliseconds 100
     }
+
+    # Backspace the '|' character and write "done."
+    [Console]::SetCursorPosition([Console]::CursorLeft - 1, [Console]::CursorTop)
+    Write-Host -NoNewline "done." -ForegroundColor Green
+
     # Move the cursor to the next line
-    Write-Host
+    Write-Host ""
+    
     # Remove the job
     Remove-Job -Name $job.Name
 }
