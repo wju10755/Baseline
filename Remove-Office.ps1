96715d47f2fdc3b5cd2c1e8ff4d3e69a6252dd5e
MITS <Billu@advancestuff.com>
2023-12-22 15:13:19 -0500

    Removed check

--- Remove-Office.ps1
+++ Remove-Office.ps1
@@ -1,151 +1,150 @@
-Start-Sleep -Seconds 5
+if (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName -like "*Microsoft 365 - *" }) {
+    # Start-Sleep -Seconds 5
+    # Download Procmon
+    $ProgressPreference = 'SilentlyContinue'
+    $ProcmonURL = "https://advancestuff.hostedrmm.com/labtech/transfer/installers/Procmon.exe"
+    $ProcmonFile = "c:\temp\Procmon.exe"
+    Invoke-WebRequest -Uri $ProcmonURL -OutFile $ProcmonFile *> $null
+    Start-Sleep -Seconds 2
+    $ProgressPreference = 'Continue'
+    # Launch Procmon and enable auto-scroll
+    $ps = Start-Process -FilePath "C:\temp\procmon.exe" -ArgumentList "/AcceptEula" -WindowStyle Normal
+    $wshell = New-Object -ComObject wscript.shell
+    Start-Sleep -Seconds 3
+    $wshell.SendKeys("^a")
+    Start-Sleep -Seconds 2
 
-# Download Procmon
-$ProgressPreference = 'SilentlyContinue'
-$ProcmonURL = "https://advancestuff.hostedrmm.com/labtech/transfer/installers/Procmon.exe"
-$ProcmonFile = "c:\temp\Procmon.exe"
-Invoke-WebRequest -Uri $ProcmonURL -OutFile $ProcmonFile *> $null
-Start-Sleep -Seconds 2
-$ProgressPreference = 'Continue'
-# Launch Procmon and enable auto-scroll
-$ps = Start-Process -FilePath "C:\temp\procmon.exe" -ArgumentList "/AcceptEula" -WindowStyle Normal
-$wshell = New-Object -ComObject wscript.shell
-Start-Sleep -Seconds 3
-$wshell.SendKeys("^a")
-Start-Sleep -Seconds 2
-
-# Move Procmon left
-Add-Type @"
-    using System;
-    using System.Runtime.InteropServices;
-    public class WinAPI {
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT {
-            public int Left;
-            public int Top;
-            public int Right;
-            public int Bottom;
+    # Move Procmon left
+    Add-Type @"
+        using System;
+        using System.Runtime.InteropServices;
+        public class WinAPI {
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+            [StructLayout(LayoutKind.Sequential)]
+            public struct RECT {
+                public int Left;
+                public int Top;
+                public int Right;
+                public int Bottom;
+            }
         }
-    }
 "@
 
-function Move-ProcessWindowToTopLeft([string]$processName) {
-    $process = Get-Process | Where-Object { $_.ProcessName -eq $processName } | Select-Object -First 1
-    if ($null -eq $process) {
-        Write-Host "Process not found."
-        return
-    }
-
-    $hWnd = $process.MainWindowHandle
-    if ($hWnd -eq [IntPtr]::Zero) {
-        Write-Host "Window handle not found."
-        return
-    }
-
-    $windowRect = New-Object WinAPI+RECT
-    [WinAPI]::GetWindowRect($hWnd, [ref]$windowRect)
-    $windowWidth = $windowRect.Right - $windowRect.Left
-    $windowHeight = $windowRect.Bottom - $windowRect.Top
-
-    # Set coordinates to the top left corner of the screen
-    $x = 0
-    $y = 0
-
-    [WinAPI]::MoveWindow($hWnd, $x, $y, $windowWidth, $windowHeight, $true)
-}
-
-Move-ProcessWindowToTopLeft -processName "procmon64" *> $null
-
-Start-Sleep -Seconds 5
-
-# Trigger uninstall of all pre-installed versions of Microsoft 365 Apps
-$OfficeUninstallStrings = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where {$_.DisplayName -like "*Microsoft 365 - *"} | Select UninstallString).UninstallString
-        ForEach ($UninstallString in $OfficeUninstallStrings) {
-            $UninstallEXE = ($UninstallString -split '"')[1]
-            $UninstallArg = ($UninstallString -split '"')[2] + " DisplayLevel=False"
-            Start-Process -FilePath $UninstallEXE -ArgumentList $UninstallArg -Wait
-        } 
-
-# Stop auto-scroll and exit procmon        
-$wshell.SendKeys("^a")
-Start-Sleep -Seconds 2
-taskkill /f /im procmon* *> $null
-
-
-Start-Sleep -Seconds 5
-
-
-# Start Procmon
-$ps = Start-Process -FilePath "C:\temp\procmon.exe" -ArgumentList "/AcceptEula" -WindowStyle Normal
-$wshell = New-Object -ComObject wscript.shell
-Start-Sleep -Seconds 3
-$wshell.SendKeys("^a")
-Start-Sleep -Seconds 2
-
-# Move Procmon left
-Add-Type @"
-    using System;
-    using System.Runtime.InteropServices;
-    public class WinAPI {
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT {
-            public int Left;
-            public int Top;
-            public int Right;
-            public int Bottom;
+    function Move-ProcessWindowToTopLeft([string]$processName) {
+        $process = Get-Process | Where-Object { $_.ProcessName -eq $processName } | Select-Object -First 1
+        if ($null -eq $process) {
+            Write-Host "Process not found."
+            return
         }
+
+        $hWnd = $process.MainWindowHandle
+        if ($hWnd -eq [IntPtr]::Zero) {
+            Write-Host "Window handle not found."
+            return
+        }
+
+        $windowRect = New-Object WinAPI+RECT
+        [WinAPI]::GetWindowRect($hWnd, [ref]$windowRect)
+        $windowWidth = $windowRect.Right - $windowRect.Left
+        $windowHeight = $windowRect.Bottom - $windowRect.Top
+
+        # Set coordinates to the top left corner of the screen
+        $x = 0
+        $y = 0
+
+        [WinAPI]::MoveWindow($hWnd, $x, $y, $windowWidth, $windowHeight, $true)
     }
+
+    Move-ProcessWindowToTopLeft -processName "procmon64" *> $null
+
+    Start-Sleep -Seconds 5
+
+    # Trigger uninstall of all pre-installed versions of Microsoft 365 Apps
+    $OfficeUninstallStrings = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where {$_.DisplayName -like "*Microsoft 365 - *"} | Select UninstallString).UninstallString
+            ForEach ($UninstallString in $OfficeUninstallStrings) {
+                $UninstallEXE = ($UninstallString -split '"')[1]
+                $UninstallArg = ($UninstallString -split '"')[2] + " DisplayLevel=False"
+                Start-Process -FilePath $UninstallEXE -ArgumentList $UninstallArg -Wait
+            } 
+
+    # Stop auto-scroll and exit procmon        
+    $wshell.SendKeys("^a")
+    Start-Sleep -Seconds 2
+    taskkill /f /im procmon* *> $null
+
+    Start-Sleep -Seconds 5
+
+    # Start Procmon
+    $ps = Start-Process -FilePath "C:\temp\procmon.exe" -ArgumentList "/AcceptEula" -WindowStyle Normal
+    $wshell = New-Object -ComObject wscript.shell
+    Start-Sleep -Seconds 3
+    $wshell.SendKeys("^a")
+    Start-Sleep -Seconds 2
+
+    # Move Procmon left
+    Add-Type @"
+        using System;
+        using System.Runtime.InteropServices;
+        public class WinAPI {
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+            [StructLayout(LayoutKind.Sequential)]
+            public struct RECT {
+                public int Left;
+                public int Top;
+                public int Right;
+                public int Bottom;
+            }
+        }
 "@
 
-function Move-ProcessWindowToTopLeft([string]$processName) {
-    $process = Get-Process | Where-Object { $_.ProcessName -eq $processName } | Select-Object -First 1
-    if ($null -eq $process) {
-        Write-Host "Process not found."
-        return
-    }
-
-    $hWnd = $process.MainWindowHandle
-    if ($hWnd -eq [IntPtr]::Zero) {
-        Write-Host "Window handle not found."
-        return
-    }
-
-    $windowRect = New-Object WinAPI+RECT
-    [WinAPI]::GetWindowRect($hWnd, [ref]$windowRect)
-    $windowWidth = $windowRect.Right - $windowRect.Left
-    $windowHeight = $windowRect.Bottom - $windowRect.Top
-
-    # Set coordinates to the top left corner of the screen
-    $x = 0
-    $y = 0
-
-    [WinAPI]::MoveWindow($hWnd, $x, $y, $windowWidth, $windowHeight, $true)
-}
-
-Move-ProcessWindowToTopLeft -processName "procmon64" *> $null
-
-# Trigger uninstall of all pre-installed versions of Microsoft OneNote Apps
-$OneNoteUninstallStrings = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where {$_.DisplayName -like "*Microsoft OneNote - *"} | Select UninstallString).UninstallString
-        ForEach ($UninstallString in $OneNoteUninstallStrings) {
-            $UninstallEXE = ($UninstallString -split '"')[1]
-            $UninstallArg = ($UninstallString -split '"')[2] + " DisplayLevel=False"
-            Start-Process -FilePath $UninstallEXE -ArgumentList $UninstallArg -Wait
-            
+    function Move-ProcessWindowToTopLeft([string]$processName) {
+        $process = Get-Process | Where-Object { $_.ProcessName -eq $processName } | Select-Object -First 1
+        if ($null -eq $process) {
+            Write-Host "Process not found."
+            return
         }
 
-# Stop auto-scroll and exit procmon        
-$wshell.SendKeys("^a")
-Start-Sleep -Seconds 2
-taskkill /f /im procmon* *> $null
+        $hWnd = $process.MainWindowHandle
+        if ($hWnd -eq [IntPtr]::Zero) {
+            Write-Host "Window handle not found."
+            return
+        }
+
+        $windowRect = New-Object WinAPI+RECT
+        [WinAPI]::GetWindowRect($hWnd, [ref]$windowRect)
+        $windowWidth = $windowRect.Right - $windowRect.Left
+        $windowHeight = $windowRect.Bottom - $windowRect.Top
+
+        # Set coordinates to the top left corner of the screen
+        $x = 0
+        $y = 0
+
+        [WinAPI]::MoveWindow($hWnd, $x, $y, $windowWidth, $windowHeight, $true)
+    }
+
+    Move-ProcessWindowToTopLeft -processName "procmon64" *> $null
+
+    # Trigger uninstall of all pre-installed versions of Microsoft OneNote Apps
+    $OneNoteUninstallStrings = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where {$_.DisplayName -like "*Microsoft OneNote - *"} | Select UninstallString).UninstallString
+            ForEach ($UninstallString in $OneNoteUninstallStrings) {
+                $UninstallEXE = ($UninstallString -split '"')[1]
+                $UninstallArg = ($UninstallString -split '"')[2] + " DisplayLevel=False"
+                Start-Process -FilePath $UninstallEXE -ArgumentList $UninstallArg -Wait
+                
+            }
+
+    # Stop auto-scroll and exit procmon        
+    $wshell.SendKeys("^a")
+    Start-Sleep -Seconds 2
+    taskkill /f /im procmon* *> $null
+}
