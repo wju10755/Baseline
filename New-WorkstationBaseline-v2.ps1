67e9c2ce981283e2f866eb7f73d8fdce3cc8e620
MITS <Billu@advancestuff.com>
2023-12-27 20:13:10 -0500

    Updated Bitlocker Module

--- New-WorkstationBaseline-v2.ps1
+++ New-WorkstationBaseline-v2.ps1
@@ -463,32 +463,55 @@ if ($null -ne $OfficeUninstallStrings) {
     Write-Warning "Skipping Pre-Installed Office Removal module due to not meeting application requirements."
 }
 
-# Registry Check
-$OfficeUninstallStrings = (Get-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -like "*Microsoft 365 - *"} | Select-Object -ExpandProperty UninstallString)
-
-# Registry Check for Pre-Installed Office
-if ($null -ne $OfficeUninstallStrings) {
-    $RPIO = "Removing Pre-Installed Office 365 Applications..."
-    foreach ($Char in $RPIO.ToCharArray()) {
-        [Console]::Write("$Char")
-        Start-Sleep -Milliseconds 50
+# Configure Bitlocker Drive Encryption
+$SBLC = "Configuring Bitlocker disk encryption..."
+foreach ($Char in $SBLC.ToCharArray()) {
+    [Console]::Write("$Char")
+    Start-Sleep -Milliseconds 50    
     }
+
+# Check Bitlocker Compatibility
+$WindowsVer = Get-WmiObject -Query 'select * from Win32_OperatingSystem where (Version like "6.2%" or Version like "6.3%" or Version like "10.0%") and ProductType = "1"' -ErrorAction SilentlyContinue
+$TPM = Get-WmiObject -Namespace root\cimv2\security\microsofttpm -Class Win32_Tpm -ErrorAction SilentlyContinue
+$BitLockerReadyDrive = Get-BitLockerVolume -MountPoint $env:SystemDrive -ErrorAction SilentlyContinue
+if ($WindowsVer -and $TPM -and $BitLockerReadyDrive) {
+
+    # Ensure the output directory exists
+    $outputDirectory = "C:\temp"
+    if (-not (Test-Path -Path $outputDirectory)) {
+        New-Item -Path $outputDirectory -ItemType Directory | Out-Null
+    }
+
+    # Create the recovery key
+    Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -RecoveryPasswordProtector | Out-Null
+
+    # Add TPM key
+    Add-BitLockerKeyProtector -MountPoint $env:SystemDrive -TpmProtector | Out-Null
+    Start-Sleep -Seconds 15 # Wait for the protectors to take effect
+
+    # Enable Encryption
+    Start-Process 'manage-bde.exe' -ArgumentList " -on $env:SystemDrive -em aes256" -Verb runas -Wait *> $null
+
+    # Get Recovery Key GUID
+    $RecoveryKeyGUID = (Get-BitLockerVolume -MountPoint $env:SystemDrive).KeyProtector | Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'} | Select-Object -ExpandProperty KeyProtectorID
+
+    # Backup the Recovery to AD
+    manage-bde.exe -protectors $env:SystemDrive -adbackup -id $RecoveryKeyGUID *> $null
+    manage-bde -protectors C: -get | Out-File "$outputDirectory\$env:computername-BitLocker.txt"
+
+    # Retrieve and Output the Recovery Key Password
+    $RecoveryKeyPW = (Get-BitLockerVolume -MountPoint $env:SystemDrive).KeyProtector | Where-Object {$_.KeyProtectortype -eq 'RecoveryPassword'} | Select-Object -ExpandProperty RecoveryPassword
+    Write-Log "Bitlocker Recovery Key: $RecoveryKeyPW"
+    [Console]::ForegroundColor = [System.ConsoleColor]::Green
+    [Console]::Write(" done.")
     [Console]::ResetColor()
-    [Console]::WriteLine()    
-
-    # Set the URL and file path variables
-    Invoke-WebRequest -Uri $config.RemoveOfficeURL -OutFile $config.RemoveOfficeScript
-    Start-Sleep -seconds 2
-    Invoke-WebRequest -Uri $config.RemoveOfficeSpinURL -OutFile $config.RemoveOfficeSpinner
-
-    if (Test-Path -Path $config.RemoveOfficeSpinner) {
-        & $config.ScrubOffice
-        & $RemoveOfficeSpinner
-        }
-
+    [Console]::WriteLine()
+    
 } else {
-    Write-Warning "Skipping Pre-Installed Office Removal module due to not meeting application requirements."
+    Write-Warning "Skipping Bitlocker Drive Encryption due to device not meeting hardware requirements."
+    #Write-Log "Only Dell systems are eligible for this bloatware removal script."
 }
+
 
 # ConnectWise Automate Agent Installation
 $file = 'c:\temp\Warehouse-Agent_Install.MSI'
