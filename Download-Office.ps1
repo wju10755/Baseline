f61fa92ede7d4c2fce786c5fcad95abc2e3bb527
MITS <Billu@advancestuff.com>
2024-02-09 10:50:45 -0500

    Updated output for local mitsadmin function.

--- Download-Office.ps1
+++ Download-Office.ps1
@@ -1,3 +1,6 @@
 $OfficePath = "c:\temp\OfficeSetup.exe"
 $OfficeURL = "https://advancestuff.hostedrmm.com/labtech/transfer/installers/OfficeSetup.exe"
 Invoke-WebRequest -OutFile $OfficePath -Uri $OfficeURL -UseBasicParsing
+
+
+
