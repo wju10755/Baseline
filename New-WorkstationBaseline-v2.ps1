80a1bcb7a47e47dd61edf48043c31a7ab05545c1
MITS <Billu@advancestuff.com>
2024-04-16 16:01:18 -0400

    Updated Dell cleanup method to skip if not found.

--- New-WorkstationBaseline-v2.ps1
+++ New-WorkstationBaseline-v2.ps1
@@ -681,29 +681,35 @@ else {
 
 Stop-Transcript *> $null
 
-# Check if the system is manufactured by Dell
-if ($manufacturer -eq "Dell Inc.") {
-    # Set the URL and file path variables
-    $SpinnerURL = "https://raw.githubusercontent.com/wju10755/Baseline/main/Dell-Spinner.ps1"
-    $SpinnerFile = "c:\temp\Dell-Spinner.ps1"
-    $DellSilentURL = "https://raw.githubusercontent.com/wju10755/Baseline/main/Dell_Silent_Uninstall-v2.ps1"
-    $DellSilentFile = "c:\temp\Dell_Silent_Uninstall.ps1"
-    Set-Location -Path "c:\temp"
-    #& $config.DellHardware
-    Invoke-WebRequest -Uri $SpinnerURL -OutFile $SpinnerFile -UseBasicParsing -ErrorAction Stop 
-    Start-Sleep -seconds 2
-    Invoke-WebRequest -Uri $DellSilentURL -OutFile $DellSilentFile -UseBasicParsing -ErrorAction Stop
+# Check if any application with "Dell" in the name is installed
+$dellApps = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "*Dell*" }
 
-    if (Test-Path -Path $SpinnerFile) {
-    #& $config.DellBloatware
-    & $SpinnerFile
-    Write-Log "Dell Bloatware Removed."
+if ($dellApps) {
+    # Check if the system is manufactured by Dell
+    if ($manufacturer -eq "Dell Inc.") {
+        # Set the URL and file path variables
+        $SpinnerURL = "https://raw.githubusercontent.com/wju10755/Baseline/main/Dell-Spinner.ps1"
+        $SpinnerFile = "c:\temp\Dell-Spinner.ps1"
+        $DellSilentURL = "https://raw.githubusercontent.com/wju10755/Baseline/main/Dell_Silent_Uninstall-v2.ps1"
+        $DellSilentFile = "c:\temp\Dell_Silent_Uninstall.ps1"
+        Set-Location -Path "c:\temp"
+        #& $config.DellHardware
+        Invoke-WebRequest -Uri $SpinnerURL -OutFile $SpinnerFile -UseBasicParsing -ErrorAction Stop 
+        Start-Sleep -seconds 2
+        Invoke-WebRequest -Uri $DellSilentURL -OutFile $DellSilentFile -UseBasicParsing -ErrorAction Stop
+
+        if (Test-Path -Path $SpinnerFile) {
+            #& $config.DellBloatware
+            & $SpinnerFile
+            Write-Log "Dell Bloatware Removed."
         }
-
+    } else {
+        Write-Warning "`nSkipping Dell debloat module due to device not meeting manufacturer requirements.`n"
+        Write-Log "Skipping Dell debloat module due to device not meeting manufacturer requirements."
+        Start-Sleep -Seconds 1
+    }
 } else {
-    Write-Warning "`nSkipping Dell debloat module due to device not meeting manufacturer requirements.`n"
-    Write-Log "Skipping Dell debloat module due to device not meeting manufacturer requirements."
-    Start-Sleep -Seconds 1
+    Write-Host "No application with 'Dell' in the name is installed."
 }
 
 # Kill procmon 
