45a3fd946e4c60fb84f03034800db8e39700f13c
MITS <Billu@advancestuff.com>
2024-04-19 13:17:40 -0400

    Updated

--- New-WorkstationBaseline-v2.ps1
+++ New-WorkstationBaseline-v2.ps1
@@ -38,7 +38,7 @@ function Print-Middle($Message, $Color = "White") {
 $Padding = ("=" * [System.Console]::BufferWidth);
 Write-Host -ForegroundColor "Red" $Padding -NoNewline;
 Print-Middle "MITS - New Workstation Baseline Script";
-Write-Host -ForegroundColor Cyan "                                                   version 10.2.6";
+Write-Host -ForegroundColor Cyan "                                                   version 10.2.7";
 Write-Host -ForegroundColor "Red" -NoNewline $Padding; 
 Write-Host "  "
 
