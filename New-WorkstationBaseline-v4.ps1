6fcf831b9491b1f26556f9ee2dfc8f6e83c3ba40
MITS <Billu@advancestuff.com>
2024-04-24 11:32:48 -0400

    Update

--- New-WorkstationBaseline-v4.ps1
+++ New-WorkstationBaseline-v4.ps1
@@ -1716,7 +1716,67 @@ Write-Delayed " done." -NewLine:$false
 [Console]::ResetColor()
 [Console]::WriteLine
 
+# Install Office 365
+$O365 = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*,
+                             HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
+Where-Object { $_.DisplayName -like "*Microsoft 365 Apps for enterprise - en-us*" }
 
+if ($O365) {
+    [Console]::ForegroundColor = [System.ConsoleColor]::Cyan
+    Write-Delayed "Existing Microsoft Office installation found." -NewLine:$false
+    [Console]::ResetColor()
+    [Console]::WriteLine()   
+} else {
+    $OfficePath = "c:\temp\OfficeSetup.exe"
+    if (-not (Test-Path $OfficePath)) {
+        $OfficeURL = "https://advancestuff.hostedrmm.com/labtech/transfer/installers/OfficeSetup.exe"
+        Write-Delayed "Downloading Microsoft Office 365..." -NewLine:$false
+        Invoke-WebRequest -OutFile $OfficePath -Uri $OfficeURL -UseBasicParsing
+        [Console]::ForegroundColor = [System.ConsoleColor]::Green
+        Write-Delayed " done." -NewLine:$false
+        [Console]::ResetColor()
+        [Console]::WriteLine()
+    }
+    # Validate successful download by checking the file size
+    $FileSize = (Get-Item $OfficePath).Length
+    $ExpectedSize = 7651616 # in bytes
+    if ($FileSize -eq $ExpectedSize) {
+        #& $config.officeNotice
+        Write-Delayed "Installing Microsoft Office 365..." -NewLine:$false
+            taskkill /f /im OfficeClickToRun.exe *> $null
+            taskkill /f /im OfficeC2RClient.exe *> $null
+            Start-Sleep -Seconds 5
+            Start-Process -FilePath $OfficePath -Wait
+        if (!(Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where {$_.DisplayName -like "Microsoft 365 Apps for enterprise - en-us"})) {
+            Write-Log "Office 365 Installation Completed Successfully."
+            [Console]::ForegroundColor = [System.ConsoleColor]::Green
+            [Console]::Write(" done.")
+            [Console]::ResetColor()
+            [Console]::WriteLine()  
+            Start-Sleep -Seconds 10
+            taskkill /f /im OfficeClickToRun.exe *> $null
+            taskkill /f /im OfficeC2RClient.exe *> $null
+            Remove-Item -Path $OfficePath -force -ErrorAction SilentlyContinue
+            } else {
+            Write-Log "Office 365 installation failed."
+            [Console]::ForegroundColor = [System.ConsoleColor]::Red
+            Write-Delayed "Microsoft Office 365 installation failed." -NewLine:$false
+            [Console]::ResetColor()
+            [Console]::WriteLine()  
+
+            }   
+    }
+    else {
+        # Report download error
+        Write-Log "Office download failed!"
+        [Console]::ForegroundColor = [System.ConsoleColor]::Red
+        Write-Delayed "Download failed or file size does not match."
+        [Console]::ResetColor()
+        [Console]::WriteLine()
+        Start-Sleep -Seconds 10
+        Remove-Item -Path $OfficePath -force -ErrorAction SilentlyContinue
+    }
+}
 
 <#
 # Install Office 365
