e94056406ba0d7fcd739513c747076b2ec3ae740
MITS <Billu@advancestuff.com>
2024-05-17 11:37:40 -0400

    Updated Dell Display Manager Uninstall method

--- Dell_Silent_Uninstall-v2.ps1
+++ Dell_Silent_Uninstall-v2.ps1
@@ -117,8 +117,23 @@ function Move-ProcessWindowToTopLeft([string]$processName) {
 }
 
 Move-ProcessWindowToTopLeft -processName "procmon64" *> $null
+
+# Remove Dell Display Manager 2.1
+$DDM2url = "https://advancestuff.hostedrmm.com/labtech/transfer/installers/Uninstall-DDM2.zip"
+$DDM2zip = "C:\temp\Uninstall-DDM2.zip"
+$DDM2dir = "C:\temp\Uninstall-DDM2"
+Invoke-WebRequest -OutFile c:\temp\Uninstall-DDM2.zip https://advancestuff.hostedrmm.com/labtech/transfer/installers/Uninstall-DDM2.zip
+
+if (Test-Path $DDM2zip) {
+    if(!(Test-Path -Path "c:\temp\Uninstall-DDM2")) { New-Item -ItemType Directory -Path "c:\temp\Uninstall-DDM2" }
+    Expand-Archive -Path $DDM2zip -DestinationPath $DDM2dir -Force
+}
+
+$null = ". .\appdeploytoolkit\AppDeployToolkitMain.ps1 | Out-Null"
+
+Start-Process PowerShell.exe -ArgumentList "-File .\Uninstall-DellDisplayManager.ps1 -DeploymentType Uninstall -DeployMode Interactive"; exit 0
+
 <#
-# Remove Dell Display Manager
 $DDMurl = "https://advancestuff.hostedrmm.com/labtech/transfer/installers/Uninstall-DDM.zip"
 $DDMzip = "C:\temp\Uninstall-DDM.zip"
 $DDMdir = "C:\temp\Uninstall-DDM"
