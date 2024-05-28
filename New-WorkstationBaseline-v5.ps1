ada5d2374852ab77811015f5f9a87d161757b940
MITS <Billu@advancestuff.com>
2024-05-28 15:48:59 -0400

    Updated module installation automation

--- New-WorkstationBaseline-v5.ps1
+++ New-WorkstationBaseline-v5.ps1
@@ -142,28 +142,32 @@ while ($true) {
 Start-Sleep -Seconds 2
 Start-Process -FilePath "powershell.exe" -ArgumentList "-file $wakeLockScriptPath" -WindowStyle Minimized
 Write-Delayed "Installing required powershell modules..." -NewLine:$false
-
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait('~');
-Write-Host
-Write-Host
-Write-Host 'Validating Availability of NuGet Package Provider, Please Wait..' 
 if ((Get-Packageprovider -Name NuGet) -eq $null) {
-    Write-Host -ForegroundColor Yellow 'NuGet package provider not found!'
-    Write-Host 'Installing NuGet 2.8.5.201 Provider, Please Wait..'
-    Install-Packageprovider -name nuget -requiredVersion 2.8.5.201 -force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Out-Null
+    #Write-Output 'NuGet package provider not found!'
+    #Write-Output 'Installing NuGet 2.8.5.201 Provider, Please Wait..'
+    try {
+        Install-Packageprovider -name nuget -requiredVersion 2.8.5.201 -force -ErrorAction Stop | Out-Null
+        #Write-Output 'NuGet package provider installed successfully.'
+    } catch {
+        #Write-Output "Failed to install NuGet package provider: $_"
     }
-Write-Host -ForegroundColor Green 'Done.'
+} else {
+    #Write-Output 'NuGet package provider is installed!'
+}
+
+#Write-Output ' Done.'
 
 
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait('~');
-Write-Host
-Write-Host
-Write-Host 'Validating Availability of PSWindows Update Module, Please Wait...' 
+#Write-Host
+#Write-Host
+#Write-Host 'Validating Availability of PSWindows Update Module, Please Wait...' 
 if ((Get-Module -Name PSWindowsUpdate) -eq $null) {
-    Write-Host -ForegroundColor Yellow 'PSWindowsUpdate module not found!'
-    Write-Host 'Installing PSWindowsUpdate Module, Please Wait...'
+    #Write-Host -ForegroundColor Yellow 'PSWindowsUpdate module not found!'
+    #Write-Host 'Installing PSWindowsUpdate Module, Please Wait...'
     Install-Module -name PSWindowsUpdate -force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Out-Null
     }
 Write-Host -ForegroundColor Green 'Done.'
