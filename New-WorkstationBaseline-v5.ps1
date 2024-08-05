c4c0e5f15d42c2792b4a598f96784126e8089306
MITS <Billu@advancestuff.com>
2024-08-05 10:07:08 -0400

    Updated Adobe Installation Process.

--- New-WorkstationBaseline-v5.ps1
+++ New-WorkstationBaseline-v5.ps1
@@ -27,10 +27,11 @@ function Print-Middle($Message, $Color = "White") {
 
 # Print Script Title
 #################################
+$ScriptVersion = "11.1.8"
 $Padding = ("=" * [System.Console]::BufferWidth);
 Write-Host -ForegroundColor "Red" $Padding -NoNewline;
 Print-Middle "MITS - New Workstation Baseline Script";
-Write-Host -ForegroundColor Cyan "                                                   version 11.1.7";
+Write-Host -ForegroundColor Cyan "                                                   version $ScriptVersion";
 Write-Host -ForegroundColor "Red" -NoNewline $Padding; 
 Write-Host "  "
 Start-Sleep -Seconds 2 
@@ -1328,53 +1329,61 @@ if ($Acrobat) {
     }
 #>
 
+
+
+
 # Acrobat Installation -v3
+# Define the URL and file path for the Acrobat Reader installer
 # Define the URL and file path for the Acrobat Reader installer
 $URL = "https://advancestuff.hostedrmm.com/labtech/transfer/installers/Reader_en_install.exe"
 $AcroFilePath = "$env:TEMP\Reader_en_install.exe"
 
-# Download the Acrobat Reader installer
-$ProgressPreference = 'SilentlyContinue'
-$response = Invoke-WebRequest -Uri $URL -Method Head
-$fileSize = $response.Headers["Content-Length"]
-$ProgressPreference = 'Continue'
-Write-Host "Downloading Adobe Acrobat Reader ($fileSize bytes)..."
+# Function to check if Adobe Acrobat Reader is installed
+function Is-AcrobatReaderInstalled {
+    try {
+        $acrobatKey = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -like "*Adobe Acrobat Reader*" }
+        if (-not $acrobatKey) {
+            throw "Adobe Acrobat Reader is not installed."
+        }
+        return $true
+    } catch {
+        Write-Host $_.Exception.Message -ForegroundColor Red
+        return $false
+    }
+}
 
-#$downloadStartTime = Get-Date
-Invoke-WebRequest -Uri $URL -OutFile $AcroFilePath -UseBasicParsing
-#$downloadEndTime = Get-Date
-
-#$downloadDuration = $downloadEndTime - $downloadStartTime
-#Write-Host "Download completed in $($downloadDuration.TotalSeconds) seconds."
-
-$FileSize = (Get-Item $AcroFilePath).Length
-$ExpectedSize = 1628608 # in bytes
-if ($FileSize -eq $ExpectedSize) {
-    # Start the silent installation of Acrobat Reader
-    Write-Host "Starting silent installation of Adobe Acrobat Reader..."
-    $installStartTime = Get-Date
-    Start-Process -FilePath $AcroFilePath -ArgumentList "/sAll /rs /rps /msi /norestart /quiet" -NoNewWindow
-
-    # Monitor the system for the active msiexec.exe process
-    Write-Host "Monitoring for msiexec.exe process..."
-    do {
-        Start-Sleep -Seconds 5
-        $msiexecProcess = Get-Process -Name msiexec -ErrorAction SilentlyContinue
-    } while ($msiexecProcess)
-
-    #$installEndTime = Get-Date
-    #$installDuration = $installEndTime - $installStartTime
-    #Write-Host "Installation completed in $($installDuration.TotalSeconds) seconds."
-
-    # Once msiexec.exe process exits, kill the Reader_en_install.exe process
-    Write-Host "msiexec.exe process has exited. Terminating Reader_en_install.exe..."
-    Stop-Process -Name Reader_en_install -Force -ErrorAction SilentlyContinue
-
-    Write-Host "Adobe Acrobat Reader installation complete."
+if (Is-AcrobatReaderInstalled) {
+    Write-Host "Existing Acrobat Reader installation found, Skipping installation."
 } else {
-    Write-Host "Download failed. File size does not match." -ForegroundColor "Red"
-    Start-Sleep -Seconds 5
-    Remove-Item -Path $AcroFilePath -Force -ErrorAction SilentlyContinue | Out-Null
+    # Download the Acrobat Reader installer
+    $ProgressPreference = 'SilentlyContinue'
+    $response = Invoke-WebRequest -Uri $URL -Method Head
+    $fileSize = $response.Headers["Content-Length"]
+    $ProgressPreference = 'Continue'
+    Write-Host "Downloading Adobe Acrobat Reader ($fileSize bytes)..."
+    Invoke-WebRequest -Uri $URL -OutFile $AcroFilePath -UseBasicParsing
+    $FileSize = (Get-Item $AcroFilePath).Length
+    $ExpectedSize = 1628608 # in bytes
+    if ($FileSize -eq $ExpectedSize) {
+        # Start the silent installation of Acrobat Reader
+        Write-Host "Starting silent installation of Adobe Acrobat Reader..."
+        $installStartTime = Get-Date
+        Start-Process -FilePath $AcroFilePath -ArgumentList "/sAll /rs /rps /msi /norestart /quiet" -NoNewWindow
+        # Monitor the system for the active msiexec.exe process
+        Write-Host "Monitoring for msiexec.exe process..."
+        do {
+            Start-Sleep -Seconds 5
+            $msiexecProcess = Get-Process -Name msiexec -ErrorAction SilentlyContinue
+        } while ($msiexecProcess)
+        # Once msiexec.exe process exits, kill the Reader_en_install.exe process
+        Write-Host "msiexec.exe process has exited. Terminating Reader_en_install.exe..."
+        Stop-Process -Name Reader_en_install -Force -ErrorAction SilentlyContinue
+        Write-Host "Adobe Acrobat Reader installation complete."
+    } else {
+        Write-Host "Download failed. File size does not match." -ForegroundColor "Red"
+        Start-Sleep -Seconds 5
+        Remove-Item -Path $AcroFilePath -Force -ErrorAction SilentlyContinue | Out-Null
+    }
 }
 
 ############################################################################################################
